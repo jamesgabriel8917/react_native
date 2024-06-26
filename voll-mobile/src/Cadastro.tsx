af8ad50215e416a5225437a622bc9cbe07933536
James Gabriel Bes Fontana <jamesgabriel8917@gmail.com>
2024-06-25 22:11:00 -0300

    adicionado mais uma pagina da tela de cadastro

--- voll-mobile/src/Cadastro.tsx
+++ voll-mobile/src/Cadastro.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Text, Box, FormControl, Input, Button, Link } from 'native-base'
+import { VStack, Image, Text, Box, FormControl, Input, Button, Link, Checkbox } from 'native-base'
 import Logo from './assets/Logo.png'
 import { TouchableOpacity } from 'react-native';
 import { Titulo } from './components/Titulo';
@@ -38,11 +38,33 @@ export default function Cadastro() {
           placeholder: 'Digite seu CEP'
         },
       ]
+    },
+    {
+      id:3,
+      Titulo:  'Para finalizar, quais são seus planos!',
+      Checkbox: [
+        {
+          id: 1,
+          value: 'Unimed',
+        },
+        {
+          id: 2,
+          value: 'Teste',
+        },
+      ]
     }
   ]
 
   function avancarSecao(){
-    setNumSecao(numSecao+1)
+    if(numSecao < secoes.length -1){
+      setNumSecao(numSecao+1)
+    }
+  }
+
+  function voltarSecao(){
+    if(numSecao>0){
+      setNumSecao(numSecao-1)
+    }
   }
 
   return (
@@ -54,16 +76,29 @@ export default function Cadastro() {
       </Titulo>
       <Box>
         {
-          secoes[numSecao].entradaTexto.map(entrada => {
-              return (<EntradaTexto
-                        label={entrada.label}
-                        placeholder={entrada.placeholder}
-                        key={entrada.id}
-                      ></EntradaTexto>)
+          secoes[numSecao].entradaTexto?.map(entrada => {
+            return (<EntradaTexto
+                      label={entrada.label}
+                      placeholder={entrada.placeholder}
+                      key={entrada.id}
+                    ></EntradaTexto>)
+          })
+        }
+      </Box>
+      <Box>
+        {
+          secoes[numSecao].Checkbox?.map(checkbox => {
+            return <Checkbox 
+            key={checkbox.id}
+            value={checkbox.value}
+            >
+              {checkbox.value}
+            </Checkbox>
           })
         }
       </Box>
       <Button onPress={() => {avancarSecao()}}>Avançar</Button>
+      {numSecao != 0 && <Button onPress={() => {voltarSecao()}}>Voltar</Button>}
 
     </VStack>
   );
