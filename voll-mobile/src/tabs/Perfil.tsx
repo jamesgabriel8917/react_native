06b6f28054eadeafeee8c577fdc04d552ddfe023
James Gabriel <jamesgabriel8917@gmail.com>
2024-07-25 23:54:03 -0300

    update do cadasstro

--- voll-mobile/src/tabs/Perfil.tsx
+++ voll-mobile/src/tabs/Perfil.tsx
@@ -5,9 +5,10 @@ import { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { pegarDadosPaciente } from "../servicos/pacienteServico";
 import { Paciente } from "../interfaces/Paciente";
+import { Botao } from "../components/Botao";
 
-export default function Perfil(){
-    const [dadosPaciente, setDadosPaciente] = useState({})
+export default function Perfil({navigation }: any){
+    const [dadosPaciente, setDadosPaciente] = useState({} as Paciente)
 
     useEffect(() => {
         async function dadosPaciente(){
@@ -18,22 +19,28 @@ export default function Perfil(){
             const resultado = pegarDadosPaciente(pacienteId)
 
             if(resultado){
-                setDadosPaciente(resultado)
+                setDadosPaciente(await resultado)
                 console.log(resultado)
             }
         }
     }, [])
+
+    function deslogar(){
+        AsyncStorage.removeItem('token')
+        AsyncStorage.removeItem('pacienteId')
+        navigation.replace('Login')
+    }
     return(
         <ScrollView flex={1}>
             <VStack flex={1} alignItems="center" p={5}>
                 <Titulo color="blue.500" mt={10}>Meu perfil</Titulo>
 
-                <Avatar source={{uri:"https://avatars.githubusercontent.com/u/44330297?v=4"}} mt={5} size="xl"/>
+                <Avatar source={{uri: dadosPaciente.imagem}} mt={5} size="xl"/>
 
                 <Titulo color="blue.500">Informações pessoais</Titulo>
-                <Titulo fontSize="lg">James Gabriel</Titulo>
-                <Text>20/01/2000</Text>
-                <Text>São Paulo</Text>
+                <Titulo fontSize="lg">{dadosPaciente.nome}</Titulo>
+                <Text>{dadosPaciente.email}</Text>
+                <Text>{dadosPaciente.endereco.estado}</Text>
 
                 <Divider mt={5}/>
                 <Titulo color="blue.500" mb={3}>Planos de saúde</Titulo>
@@ -42,6 +49,10 @@ export default function Perfil(){
                             <Text key={index}>{plano}</Text>
                         ))
                     }
+
+                    <Botao onPress={deslogar}>
+                        Deslogar
+                    </Botao>
             </VStack>
         </ScrollView>
     )
